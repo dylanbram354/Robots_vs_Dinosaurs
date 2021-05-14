b70637150125eaf5a3f7e379c667ab17637122e2
dylanbram354 <dylanbram354@gmail.com>
2021-05-14 10:34:06 -0500

    added different amounts of energy drain with each user-chosen robot weapon

--- Robos vs dinos - pycharm/robot.py
+++ Robos vs dinos - pycharm/robot.py
@@ -10,17 +10,17 @@ class Robot:
 
     def attack(self, dinosaur):
         dinosaur.health -= self.weapon.attack_power
-        self.power_level -= 10
+        self.power_level += self.weapon.energy_drain
 
     def choose_weapon(self):
-        option_1 = Weapon('laser gun', 20)
-        option_2 = Weapon('rocket launcher', 30)
-        option_3 = Weapon('sword', 25)
+        option_1 = Weapon('laser gun', 20, -10)
+        option_2 = Weapon('rocket launcher', 30, -20)
+        option_3 = Weapon('sword', 25, -15)
         options = [option_1, option_2, option_3]
         print(f"Choose this robot's weapon:")
         i = 1
         while i <= len(options):
-            print(f"Option {i}: {options[i-1].type}, attack power {options[i-1].attack_power}")
+            print(f"Option {i}: {options[i-1].type}, attack power {options[i-1].attack_power}, energy drain {options[i-1].energy_drain}")
             i += 1
         user_choice = input('Enter your choice. For Option 1, enter 1. Etc... ')
         user_choice = int(user_choice)-1
--- Robos vs dinos - pycharm/weapon.py
+++ Robos vs dinos - pycharm/weapon.py
@@ -1,4 +1,5 @@
 class Weapon:
-    def __init__(self, type, attack_power):
+    def __init__(self, type, attack_power, energy_drain = -10):
         self.type = type
         self.attack_power = attack_power
+        self.energy_drain = energy_drain
